cb568f0d77d3c9b012f55b52ab100b6ee5910818
Thorondhor <38564454+Thorondhor@users.noreply.github.com>
2018-04-24 13:50:53 +0200

    Update fbot.js

--- fbot.js
+++ fbot.js
@@ -153,11 +153,12 @@ bot.on("message", async message => {
           return message.reply(`Indiquez le nombre de messages à supprimer. \n Utilisez : ${config.prefix} aspi <nombre entre 2 et 100>`); //\n means new line.
 } 
 const fetched = await message.channel.fetchMessages({count: deleteCount});
-   message.channel.bulkDelete(fetched);
+   message.channel.bulkDelete(fetched)
+  .catch(error => message.reply(`problème en passant l'apsirateur: ${error}`));
 console.log(fetched.size + ' messages trouvés, suppression...'); // moi c'est simple sans message de confirmation, j'y crois pas^^
 message.reply(`Aspirateur passé avec succès. \n Total des messages supprimés (dont la commande): ${fetched.size}`)
-       if(!fetched)
-         return message.reply("erreur en tentant de passer l'aspirateur")
+  }
+});
 
 }
 
